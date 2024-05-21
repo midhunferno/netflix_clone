5ce0c0e760709d61974c68bbbf68ff5f9fe7c6ee
midhunferno <midhunferno819@gmail.com>
2024-05-21 12:38:24 +0530

    modifide

--- customer/models.py
+++ customer/models.py
@@ -2,9 +2,24 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid
 # Create your models here.
+
 AGE_CHOICE=(
     ('All','All'),
     ('Kids','Kids'),    
 )
-class Costom (AbstractUser):
+
+class CustomUser(AbstractUser):
     profile=models.ManyToManyField('profile',blank=True)
+    conform_password=models.TextField()
+
+    def __str__(self) -> str:
+        return self.username
+
+class Profiles(models.Model):
+    name=models.CharField(max_length=30)
+    age_limit=models.CharField(choices=AGE_CHOICE,max_length=30)
+    uuid=models.UUIDField(default=uuid.uuid4)
+
+    def __str__(self) -> str:
+        return self.name
+    
