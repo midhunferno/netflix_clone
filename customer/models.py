73120d259d33f2eea673609a382d7daf7086601e
midhunferno <midhunferno819@gmail.com>
2024-05-22 11:24:18 +0530

    compleat registration

--- customer/models.py
+++ customer/models.py
@@ -12,9 +12,6 @@ class CustomUser(AbstractUser):
     profile=models.ManyToManyField('profile',blank=True)
     conform_password=models.TextField()
 
-    def __str__(self) -> str:
-        return self.username
-
 class Profile(models.Model):
     name=models.CharField(max_length=30)
     age_limit=models.CharField(choices=AGE_CHOICE,max_length=30)
--- customer/views.py
+++ customer/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import CustomUser
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 def signup(request):
+    if request.method == 'POST':
+        username=request.POST['Username']
+        email=request.POST['email']
+        password=request.POST['password']
+        conform_password=request.POST['password2']
+        if password != conform_password:
+            err="Passwords do not match"
+            return render(request,'signup.html',{'err':err})
+        elif CustomUser.objects.filter(username=username).exists():
+            err1="This username already exists"
+            return render(request,'signup.html',{'err':err1})
+        else:
+         user=CustomUser.objects.create(
+            username=username,
+            email=email,
+            password=make_password(password),
+            conform_password=make_password(conform_password)          
+        )
+        user.save()
+        return redirect('login')
     return render(request,'signup.html')
 
 def log_in(request):
