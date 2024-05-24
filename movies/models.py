9b88dd03e84e277fa8e4f64d2ceca92cb2d93e88
midhunferno <midhunferno819@gmail.com>
2024-05-24 21:54:26 +0530

    creat movie modeles.py

--- movies/models.py
+++ movies/models.py
@@ -1,10 +1,27 @@
 from django.db import models
+import uuid
+from customer.models import AGE_CHOICE
 
 # Create your models here.
 
-# MOVIE_CHOICE=(
-#     ('seasonal','seasonal'),
-#     ('single','single')
-# )
-# class Movies(models.Model):
-#     pass
+MOVIE_CHOICE=(
+    ('seasonal','seasonal'),
+    ('single','single')
+)
+class Movies(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    uuid = models.UUIDField(default=uuid.uuid4)
+    type = models.CharField(choices=MOVIE_CHOICE,max_length=10)
+    video = models.ManyToManyField('videos')
+    image = models.ImageField(upload_to='cover')
+    agel_limit = models.CharField(choices=AGE_CHOICE,max_length=15)
+    def __str__(self) -> str:
+        return self.title
+
+class videos(models.Model):
+    title=models.CharField(max_length=100)
+    file=models.FileField(upload_to='movies') 
+    def __str__(self) -> str:
+        return self.title   
--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    
+def index(request):    
     return render(request,'index.html')
+def movie_list(request):
+    return render(request,'movie_list.html')
